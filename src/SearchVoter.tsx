2085b579bd714e5cfee316b785c9e72f4e2dcc2a
Ally Muellner <123662184+amuellner@users.noreply.github.com>
2024-12-07 15:15:26 -0600

    Add files via upload

---
+++ src/SearchVoter.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './SearchVoter.css';
+import { useNavigate } from 'react-router-dom';
+
+const SearchVoter: React.FC = () => {
+    const navigate = useNavigate(); // Hook to navigate between routes
+
+    const handleRegister = (e: React.FormEvent) => {
+        e.preventDefault();
+        navigate('/voter-results');
+        
+    };
+
+    return (
+        <div className="my-search-container">
+            <button className="back-button" onClick={() => navigate('/')}>
+                Back to Registration
+            </button>
+            <h2>Search Voter</h2>
+            <form onSubmit={handleRegister}>
+                <label>
+                    Voter ID:
+                    <input type="text" placeholder="Enter Voter ID" required />
+                </label>
+                <label>
+                    Date of Birth:
+                    <input type="date" required />
+                </label>
+                <button className="my-search-button" onClick={() => navigate('/voter-results')}>
+                    Search
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default SearchVoter;
