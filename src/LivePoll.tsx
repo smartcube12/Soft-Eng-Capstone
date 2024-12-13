ea14f695eebda9bd9b8ac6b1dd6b6fecb1c82c96
Ally Muellner <123662184+amuellner@users.noreply.github.com>
2024-12-12 22:12:19 -0600

    Add files via upload

---
+++ src/LivePoll.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './LivePoll.css';
+import { useNavigate } from 'react-router-dom';
+
+interface IVote {
+    [key: string]: number;
+}
+
+const LivePoll: React.FC = () => {
+    const navigate = useNavigate(); // Hook to navigate between routes
+
+    const [trackVotes, setVotes] = useState<IVote>({
+        HOR1:0,
+        HOR2:0,
+        Judge1:0,
+        Judge2:0,
+        Mayor1:0,
+        Mayor2:0,
+        Measure1_1:0,
+        Measure1_2:0,
+        Measure2_1:0,
+        Measure2_2:0,
+        Measure3_1:0,
+        Measure3_2:0,
+        President1:0,
+        President2:0, 
+        Senate1:0,
+        Senate2:0,
+        VicePresident1:0,
+        VicePresident2:0}); //placeholders
+
+    return (
+        <div className="my-poll-container">
+            <button className="back-button" onClick={() => navigate('/voter-information')}>
+                Back to Search
+            </button>
+            <h2>Live Poll</h2>
+            <form>
+                <div className ="my-choice">
+                {Object.keys(trackVotes).map((choice) => (
+                    <div key={choice}
+                    className = "my-choice">
+                <label>{choice}</label>
+                {}
+                <span>{trackVotes[choice as keyof IVote]} votes</span>
+                </div>
+                ))}
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default LivePoll;
